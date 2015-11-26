c59e632def89d1bfc2c0ecf43caf0ffe0f0a74a0
Nicholas Del Grosso <delgrosso.nick@gmail.com>
2015-11-26 13:05:45 +0100

    added Scaler interaction

--- interactions.py
+++ interactions.py
@@ -5,12 +5,12 @@ from ratcave.utils import timers
 
 class Spinner(graphics.Physical):
 
-    def __init__(self, velocity=1., axis=1, *args, **kwargs):
+    def __init__(self, spin_velocity=1., axis=1, *args, **kwargs):
         """Spins in direction "axis" with speed "velocity" when Spinner.update_physics(dt) is called!"""
         super(Spinner, self).__init__(*args, **kwargs)
 
         self.velocity = 0.
-        self.spin_velocity = velocity
+        self.spin_velocity = spin_velocity
         self.axis = axis
         self.timer = timers.countdown_timer(0)
 
@@ -31,6 +31,7 @@ class Spinner(graphics.Physical):
 class Jumper(graphics.Physical):
 
     def __init__(self, jump_velocity=2., gravity_coeff=-2.2, *args, **kwargs):
+        """Jumps with jump_velocity, coming back down at rate gravity_coeff."""
         super(Jumper, self).__init__(*args, **kwargs)
 
         self.floor_height = self.position[1]
@@ -53,6 +54,34 @@ class Jumper(graphics.Physical):
             self.position[1] += (self.velocity * dt)
         else:
             self.velocity = 0.
+
+
+class Scaler(graphics.Physical):
+
+    def __init__(self, end_scale=.2, scale_velocity = 1., *args, **kwargs):
+        """Grows and Shrinks between its scale and the end_scale endpoints with speed scale_velocity."""
+
+        super(Scaler, self).__init__(*args, **kwargs)
+        self.scale_endpoints = tuple(sorted(self.scale, end_scale))
+        self.scale_velocity = scale_velocity
+        self.scale_direction = -1 if end_scale < self.scale else 1.
+        self.timer = timers.countdown_timer(0)
+
+    def start(self):
+        self.timer = timers.countdown_timer(2.)
+
+    def update(self, dt):
+
+        if self.timer.next() > 0.:
+
+            # When it reaches the end of the scale range, flip the direction
+            if self.scale < self.scale_endpoints[0] or self.scale > self.scale_endpoints[1]:
+                self.scale_direction *= -1
+
+            # Set New Scale
+            self.scale += self.scale_direction * self.scale_velocity * dt
+
+
 
 
 
