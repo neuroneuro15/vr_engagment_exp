03634ba86c513485ec790bddbc5b3a2e6d875185
Nicholas A. Del Grosso, Lab Comp <delgrosso.nick@gmail.com>
2015-11-27 01:35:46 +0100

    spinner works, too!

--- interactions.py
+++ interactions.py
@@ -1,10 +1,11 @@
 import ratcave.graphics as graphics
 from ratcave.utils import timers
+import random
 
 
 class Spinner(graphics.Physical):
 
-    def __init__(self, spin_velocity=1., axis=1, *args, **kwargs):
+    def __init__(self, spin_velocity=45., axis=1, *args, **kwargs):
         """Spins in direction "axis" with speed "velocity" when Spinner.update_physics(dt) is called!"""
         super(Spinner, self).__init__(*args, **kwargs)
 
@@ -14,22 +15,21 @@ class Spinner(graphics.Physical):
         self.timer = timers.countdown_timer(0)
 
     def start(self):
-        self.velocity = self.spin_velocity
-        self.timer = timers.countdown_timer(2.)
+        if self.velocity:
+            self.timer = timers.countdown_timer(2.)
+        else:
+            self.velocity = self.spin_velocity * random.choice([1., -1.])
+
 
     def update(self, dt):
         # Keep spinning if timeout hasn't completed.
         if self.timer.next() > 0.:
-            rotation = list(self.rotation)
-            #rotation[self.axis] += self.velocity * dt
-            setattr(self, 'xyz'[self.axis], getattr(self, 'xyz'[self.axis] + self.velocity * dt))
-
-            self.rotation = rotation
+            self.rot_y += self.velocity * dt
 
 
 class Jumper(graphics.Physical):
 
-    def __init__(self, jump_velocity=2., gravity_coeff=-5.5, *args, **kwargs):
+    def __init__(self, jump_velocity=1., gravity_coeff=-5.5, *args, **kwargs):
         """Jumps with jump_velocity, coming back down at rate gravity_coeff."""
         super(Jumper, self).__init__(*args, **kwargs)
 
@@ -58,11 +58,11 @@ class Jumper(graphics.Physical):
 
 class Scaler(graphics.Physical):
 
-    def __init__(self, end_scale=.2, scale_velocity=1., *args, **kwargs):
-        """Grows and Shrinks between its scale and the end_scale endpoints with speed scale_velocity."""
+    def __init__(self, end_scale=.5, scale_velocity=.01, *args, **kwargs):
+        """Grows and Shrinks between its scale and the end_scale (relative to its current scale) endpoints with speed scale_velocity."""
 
         super(Scaler, self).__init__(*args, **kwargs)
-        self.scale_endpoints = tuple(sorted((self.scale, end_scale)))
+        self.scale_endpoints = tuple(sorted((self.scale, self.scale * end_scale)))
         self.scale_velocity = scale_velocity
         self.scale_direction = -1 if end_scale < self.scale else 1.
         self.timer = timers.countdown_timer(0)
