3811880e4c42a62bb0c6637f2a075d3e809d8a92
Nicholas A. Del Grosso, Lab Comp <delgrosso.nick@gmail.com>
2015-11-29 17:15:21 +0100

    refactoed metadata dict as being the main parameter dict

--- engagement_exp.py
+++ engagement_exp.py
@@ -16,19 +16,12 @@ import natnetclient
 # Script
 
 # Note: Collect Metadata (subject, mainly, and Session Parameters) for the log
-exp_name = 'VR_Engagement'
-nPhases = 2
-total_phase_secs = 60.#5 * 60.  # 5 minutes
-corner_idx = random.randint(1, 5)  # Select which corner everything appears in.
-interaction_level = 2#random.randint(0, 3)  # Three different levels
-interaction_distance = .15  # In meters (I think)
-
-metadata = {'Experiment:': exp_name,
-            'Total Phases: ': nPhases,
-            'Phase Time (secs)': total_phase_secs,
-            'Corner ID where Objects Appear:': corner_idx,
-            'Interactivity Amount (0-2)': interaction_level,
-            'Rat-Object Distance Where Interaction Activates (meters)': interaction_distance,
+metadata = {'Experiment': 'VR_Engagement',
+            'nPhases': 2,
+            'Phase Time':  60.,#5 * 60.  # 5 minutes,
+            'Corner ID': random.randint(1, 5),  # Select which corner everything appears in.
+            'Interaction Level': 2, #random.randint(0, 3)  # Three different levels
+            'Interaction Distance': .15,  # In meters (I think)
             'Experimenter': 'Nicholas A. Del Grosso'}
 
 info = {'Rat': ['Test', 'Nessie', 'FuzzPatch', 'FlatWhite', 'Bridger']}
@@ -57,7 +50,7 @@ vir_arena.load_texture(graphics.resources.img_uvgrid)
 reader =graphics.WavefrontReader(os.path.join('obj', 'VR_Playground.obj'))
 mesh_pos = {'Center': None, 'Side': None, 'Corner': None}
 for coord in mesh_pos:
-    mesh_name = 'Pos' + coord + str(corner_idx) if coord is not 'Center' else 'Pos' + coord
+    mesh_name = 'Pos' + coord + str(metadata['Corner ID']) if coord is not 'Center' else 'Pos' + coord
     mesh = reader.get_mesh(mesh_name)
     mesh.local.y += .015
     mesh_pos[coord] = mesh.local.position # TODO: Make sure this is the correct position
@@ -68,11 +61,11 @@ del reader
 
 # Note: Only for interaction levels 1 and 2 (No Virtual Meshes in interaction level 0)
 mesh_groups = []
-if interaction_level > 0:
+if metadata['Interaction Level'] > 0:
 
     # Note: Import Mesh Objects (randomly chosen) and put in groups of three, one group for each phase
     vir_reader = graphics.WavefrontReader(os.path.join('obj', 'NOP_Primitives.obj'))
-    for phase in range(nPhases):
+    for phase in range(metadata['nPhases']):
         meshes = []
         for pos_coords in mesh_pos.values():
             # Read Mesh
@@ -91,7 +84,7 @@ if interaction_level > 0:
         mesh_groups.append(meshes)
 
     # Note: Interaction Level 2: Assign Object Properties (based on Interaction Level)
-    if interaction_level > 1:
+    if metadata['Interaction Level'] > 1:
         interact_opts = [interactions.Spinner, interactions.Scaler, interactions.Jumper]
         for group in mesh_groups:
             for mesh in group:
@@ -100,25 +93,25 @@ if interaction_level > 0:
 
 # Note: Build Scenes (1st half, 2nd half) and window
 active_scene = graphics.Scene([arena], camera=graphics.projector, light=graphics.projector, bgColor=(0., 0., .2, 1.))
-vir_scenes = [graphics.Scene([vir_arena], light=graphics.projector) for phase in range(nPhases)]
-if interaction_level > 0:
+vir_scenes = [graphics.Scene([vir_arena], light=graphics.projector) for phase in range(metadata['nPhases'])]
+if metadata['Interaction Level'] > 0:
     for meshes, scene in zip(mesh_groups, vir_scenes):
         scene.meshes.extend(meshes)
 
 window = graphics.Window(active_scene, fullscr=True, screen=1)
 
 # Note: Wait for recording to start in Motive before starting the session.
-tracker.set_take_file_name(exp_name +  datetime.datetime.today().strftime('_%Y-%m-%d_%H-%M-%S') + '.take')
+tracker.set_take_file_name(metadata['Experiment'] + datetime.datetime.today().strftime('_%Y-%m-%d_%H-%M-%S') + '.take')
 tone.play()  # Just to get the experimenter's attention
 # tracker.wait_for_recording_start()
 
 # Note: Main Experiment Loop
-with graphics.Logger(scenes=[active_scene], exp_name=exp_name, log_directory=os.path.join('.', 'logs'),
+with graphics.Logger(scenes=[active_scene], exp_name=metadata['Experiment'], log_directory=os.path.join('.', 'logs'),
                      metadata_dict=metadata) as logger:
 
     import pdb
     pdb.set_trace()
-    for phase in xrange(nPhases):
+    for phase in xrange(metadata['nPhases']):
 
         window.virtual_scene = vir_scenes[phase]
         for mesh in window.virtual_scene.meshes:
@@ -128,7 +121,7 @@ with graphics.Logger(scenes=[active_scene], exp_name=exp_name, log_directory=os.
 
         logger.write('Start of Phase {}'.format(phase))
 
-        for _, dt in itertools.izip(ratcave.utils.timers.countdown_timer(total_phase_secs, stop_iteration=True),
+        for _, dt in itertools.izip(ratcave.utils.timers.countdown_timer(metadata['Phase Time'], stop_iteration=True),
                                     ratcave.utils.timers.dt_timer()):
 
             # Note: Update Data
@@ -141,7 +134,7 @@ with graphics.Logger(scenes=[active_scene], exp_name=exp_name, log_directory=os.
             for mesh in window.virtual_scene.meshes + [arena]:
 
                 # Activate the mesh's custom physics if the rat gets close
-                if np.linalg.norm(np.subtract(window.virtual_scene.camera.position, mesh.position)[::2]) < interaction_distance:
+                if np.linalg.norm(np.subtract(window.virtual_scene.camera.position, mesh.position)[::2]) < metadata['Interaction Distance']:
                     # tone.play()
                     mesh.local.start()
 
